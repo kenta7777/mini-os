2577f7fcd4a14f53de7748db74e78b24ddbc1a50
kenta7777 <k.hasegw7@gmail.com>
2021-03-30 01:31:11 +0900

    [WIP] Implement exception handler for breakpoint

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,16 @@
 #![no_std]
 
-use x86_64::structures::idt::InterruptDescriptorTable;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+//use crate::println;
 
 pub fn init_idt() {
+    // Create interrupt descriptor table
     let mut idt = InterruptDescriptorTable::new();
+}
+
+// Breakpoint exception handler function
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame)
+{
+    //TODO: Implement vga buffer
+    //println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
