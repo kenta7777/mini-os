8302dffb0bd52b9af95396559a2c7407aa1cca01
kenta7777 <k.hasegw7@gmail.com>
2021-05-06 00:23:43 +0900

    [hw-interrupts] Refactor serial.rs

--- src/serial.rs
+++ src/serial.rs
@@ -2,18 +2,18 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use uart_16550::SerialPort;
 
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
 #[doc(hidden)]
 pub fn _print(args: ::core::fmt::Arguments) {
     use core::fmt::Write;
     use x86_64::instructions::interrupts;
-
-    lazy_static! {
-        pub static ref SERIAL1: Mutex<SerialPort> = {
-            let mut serial_port = unsafe { SerialPort::new(0x3F8) };
-            serial_port.init();
-            Mutex::new(serial_port)
-        };
-    }
 
     interrupts::without_interrupts(|| {
         SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
