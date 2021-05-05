c581a165edfe957e51c8a744bd9b89dc09508d1b
kenta7777 <k.hasegw7@gmail.com>
2021-05-06 00:57:26 +0900

    [hw-interrupts] Enable to write numbers

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -19,6 +19,7 @@ pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPic
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
@@ -43,6 +44,8 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+        idt[InterruptIndex::Keyboard.as_usize()]
+            .set_handler_fn(keyboard_interrupt_handler);
         idt
     };
 }
@@ -72,5 +75,39 @@ extern "x86-interrupt" fn timer_interrupt_handler(
     print!(".");
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
+    }
+}
+
+// Keyboard Interrupt
+extern "x86-interrupt" fn keyboard_interrupt_handler(
+    _stack_frame: InterruptStackFrame)
+{
+    use x86_64::instructions::port::Port;
+
+    // Get a byte from keyboard data port through I/O port(0x60) 
+    let mut port = Port::new(0x60);
+    let scancode: u8 = unsafe {port.read()};
+    
+    let key = match scancode {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+
+    if let Some(key) = key {
+        print!("{}", key);
+    }
+    
+    unsafe {
+        PICS.lock()
+        .notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
